8219907e6780bf74f501664a57befa46fd3f40d8
Sounak Saha <sounaksaha@Mac.lan>
2025-06-14 15:19:21 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthContext";
 import  "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/login">
       <AuthProvider>
         <App />
       </AuthProvider>
